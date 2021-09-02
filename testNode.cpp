fae92adbb50f61d06e30b1975faeef81d3108089
Brandon Benthal <bb144@students.uwf.edu>
2021-09-02 18:40:37 -0500

    implemented copy constructor

--- testNode.cpp
+++ testNode.cpp
@@ -2,13 +2,6 @@
 
 #include "node.cpp"
 
-void testDefaultConstructor();
-
-	int main() {
-		testDefaultConstructor();
-		return 0;
-	}
-
 	void testDefaultConstructor() {
 		const int NUMTASKS = 3;
 		int passedTasks = 0;
@@ -42,3 +35,51 @@ void testDefaultConstructor();
 			<< passedTasks << " / " << NUMTASKS 
 			<< " tasks successful.\n";
 	}
+
+	void testCopyConstructor() {
+                const int NUMTASKS = 3;
+                int passedTasks = 0;
+
+		node* c = new node();
+		node* d = new node();
+		
+		c->setID("barbera");
+		c->setPass("meettheflinstones");
+		c->setNext(d);
+
+		node* n = new node(c);
+
+
+                std::cout << "Running copy constructor test:\n";
+		if (n->getID() == "barbera") {
+                        passedTasks++;
+                }
+                else {
+                        std::cout << "Copy constructor ID test failed\n";
+                }
+
+                if (n->getPass() == "meettheflinstones") {
+                        passedTasks++;
+                }
+                else {
+                        std::cout << "Copy constructor Pass test failed\n";
+                }
+
+                if (n->getNext() == d) {
+                        passedTasks++;
+                }
+                else {
+			std::cout << "Copy constructor Next test failed\n";
+		}
+
+		std::cout << "Copy Constructor Test completed. "
+                        << passedTasks << " / " << NUMTASKS
+                        << " tasks successful.\n";
+	}
+
+	int main() {
+                testDefaultConstructor();
+                testCopyConstructor();
+                return 0;
+        }
+
