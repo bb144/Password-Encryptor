34bb292335629832fd55e8f995b180fe144363e8
Brandon Benthal <bb144@students.uwf.edu>
2021-09-02 19:53:46 -0500

    Implemented showAll and add_node functionality

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -13,11 +13,16 @@ LinkedList::~LinkedList() {
 }
 
 void LinkedList::add_node(node* user) {
-	
+	user->setNext(head);
+	head = user;
 }
 
 void LinkedList::showAll() {
-
+	node* curr = head;
+	while(curr != nullptr) {
+		std::cout << curr->getID() << " " << curr->getPass() << "\n";
+		curr = curr->getNext();
+	}
 }
 
 node* LinkedList::getHead() {
